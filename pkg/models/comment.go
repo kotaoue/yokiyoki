1485096a016fc768dd5ec2e489ba96e8b76ceb6c
kotaoue <kotaoue@gmail.com>
2026-03-11 19:34:43 +0900

    Remove extra spacing in Comment struct
    Trim unnecessary spaces in the Comment.Type field comment in pkg/models/comment.go to improve formatting and maintain consistent code style.

--- pkg/models/comment.go
+++ pkg/models/comment.go
@@ -5,7 +5,7 @@ import "time"
 // Comment represents a comment (or initial body) in a PR or issue conversation.
 type Comment struct {
 	Repository string    `json:"repository"`
-	Type       string    `json:"type"`       // "pr" or "issue"
+	Type       string    `json:"type"` // "pr" or "issue"
 	Number     int       `json:"number"`
 	Title      string    `json:"title"`
 	Author     string    `json:"author"`
